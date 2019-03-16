6a3ba8c6c9cd99aa381daef44e364231522d45a5
atzoriandrea <37103069+atzoriandrea@users.noreply.github.com>
2019-03-16 01:09:14 +0100

    now

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,23 +8,26 @@ public class PlayerController : MonoBehaviour
     public float JumpForce;
     Transform t;
     private Rigidbody rb;
-    void Start(){
+    void Start()
+    {
         rb = GetComponent<Rigidbody>();
-        
+
         MoveSpeed = 1.5f;
-        JumpForce = 1.5f;
+        JumpForce = 5.5f;
     }
-    void Update(){
+    void Update()
+    {
 
     }
 
-    void FixedUpdate(){
-        var camera = Camera.main;
-        var forward = camera.transform.forward;    
-        forward.y = 0f;//
-        var desiredMoveDirection = forward * Input.GetAxisRaw("Vertical") + transform.right * Input.GetAxisRaw("Horizontal");
+    void FixedUpdate()
+    {
+        Vector3 targetDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        targetDirection = Camera.main.transform.TransformDirection(targetDirection);
+        targetDirection.y = 0.0f;
+        //var desiredMoveDirection = forward * Input.GetAxisRaw("Vertical") + transform.right * Input.GetAxisRaw("Horizontal");
 
-        transform.Translate(desiredMoveDirection * MoveSpeed * Time.deltaTime);
+        transform.Translate(targetDirection * MoveSpeed * Time.deltaTime);
         if (Input.GetKey(KeyCode.LeftShift))
         {
             MoveSpeed = 2.3f;
@@ -36,5 +39,5 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown("space"))
             rb.AddForce(transform.up * JumpForce);
     }
-  
+
 }
