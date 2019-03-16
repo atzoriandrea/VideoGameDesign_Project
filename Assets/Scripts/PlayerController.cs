ab720df8d3cc7c36b15baf160c035ea67ef57cf3
atzoriandrea <37103069+atzoriandrea@users.noreply.github.com>
2019-03-16 04:42:31 +0100

    Camera funzionante

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,89 +4,55 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
 
-    public int Speed { get; set; }
-    public int JumpForce { get; set; } 
+    public int Speed;
+    public int JumpForce;
     Transform t;
-<<<<<<< HEAD
-    private Rigidbody rb;
+    Vector3 jumpVector;
+    //Vector3 fromCameraToMe;
+    //public GameObject mainCamera;
+    public Rigidbody _rigidB;
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-
-        MoveSpeed = 1.5f;
-        JumpForce = 5.5f;
-=======
-    Vector3 jumpVector;
-    private Rigidbody _rigidB;
-    void Start(){
         _rigidB = GetComponent<Rigidbody>();
-        Speed = 5;
-        JumpForce = 20;
-
->>>>>>> ac087ff88bde15f5069267c6e4fa9f5f28f0e01d
+        Speed = 3;
+        //mainCamera = ;
+        JumpForce = 420;
     }
     void Update()
     {
-
         jumpVector.x = (Input.GetAxis("Horizontal") * JumpForce);
         jumpVector.y = JumpForce;
         jumpVector.z = (Input.GetAxis("Vertical") * JumpForce);
-        if (Input.GetKey("w"))
-        {
-            _rigidB.AddForce(Vector3.forward * Speed);
-        }
-        if (Input.GetKey("s"))
-        {
-            _rigidB.AddForce(Vector3.back * Speed);
-        }
-        if (Input.GetKey("d"))
-        {
-            _rigidB.AddForce(Vector3.right * Speed);
-        }
-        if (Input.GetKey("a"))
-        {
-            _rigidB.AddForce(Vector3.left * Speed);
-        }
     }
 
-<<<<<<< HEAD
     void FixedUpdate()
     {
-        Vector3 targetDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
-        targetDirection = Camera.main.transform.TransformDirection(targetDirection);
-        targetDirection.y = 0.0f;
-        //var desiredMoveDirection = forward * Input.GetAxisRaw("Vertical") + transform.right * Input.GetAxisRaw("Horizontal");
-
-        transform.Translate(targetDirection * MoveSpeed * Time.deltaTime);
-=======
-    void FixedUpdate(){
-
-        var forward = Camera.main.transform.forward;    
-        forward.y = 0;//
-        var desiredMoveDirection = forward * Input.GetAxisRaw("Vertical") + transform.right * Input.GetAxisRaw("Horizontal");
-
-        transform.Translate(desiredMoveDirection * Speed * Time.deltaTime);
->>>>>>> ac087ff88bde15f5069267c6e4fa9f5f28f0e01d
-        if (Input.GetKey(KeyCode.LeftShift))
+        Vector3 fromCameraToMe = transform.position - Camera.main.transform.position;
+        fromCameraToMe.y = 0;
+        if (Input.GetAxisRaw("Vertical") != 0.0 || Input.GetAxisRaw("Horizontal") != 0.0)
         {
-            Speed = 12;
+            transform.Translate(fromCameraToMe * Input.GetAxisRaw("Vertical")+ Camera.main.transform.right * (Input.GetAxisRaw("Horizontal")*-1) * Speed * Time.deltaTime);
         }
         else
         {
-            Speed = 5;
+            _rigidB.velocity = Vector3.zero;
+        }
+       
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Speed = 7;
+        }
+        else
+        {
+            Speed = 3;
         }
         if (Input.GetKeyDown("space"))
         {
+            _rigidB.AddForce(new Vector3(0, 10 * _rigidB.mass, 0), ForceMode.Impulse);
 
-            _rigidB.MovePosition(_rigidB.position + jumpVector * Time.deltaTime);
         }
+        
 
     }
-<<<<<<< HEAD
 
 }
-=======
-  
-}
-
->>>>>>> ac087ff88bde15f5069267c6e4fa9f5f28f0e01d
