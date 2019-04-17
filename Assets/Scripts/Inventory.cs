e3268edba473e4a1ac9ec56cefd12f70e9495125
Davide <davide.corda95@gmail.com>
2019-04-17 22:49:07 +0200

    Cambio armi in base al bottone inventario premuto

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -6,8 +6,8 @@ using UnityEngine.UI;
 public class Inventory : MonoBehaviour {
     private int selected;
     Image sword, secondary, potion, bendages;
+    public GameObject weapon1, weapon2, item1, item2;
     UnityEngine.Color sw, snd, pot, ben;
-    float[] colors = new float[3];
     // Use this for initialization
     void Start () {
         selected = 1;
@@ -25,37 +25,67 @@ public class Inventory : MonoBehaviour {
     void Update() {
         if (Input.GetKeyDown("1")) {
             selected = 1;
-            sword.color = new Color(sw.r + 0.2f, sw.g + 0.2f, sw.b + 0.2f);
-            secondary.color = snd;
-            potion.color = pot;
-            bendages.color = ben;
+            Sword();
         }
         if (Input.GetKeyDown("2")) { 
-             selected = 2;
-            sword.color = sw;
-            secondary.color = new Color(snd.r + 0.2f, snd.g + 0.2f, snd.b + 0.2f);
-            potion.color = pot;
-            bendages.color = ben;
+            selected = 2;
+            SecondWeapon();
         }
         if (Input.GetKeyDown("3"))
         {
             selected = 3;
-            sword.color = sw;
-            secondary.color = snd;
-            potion.color = new Color(pot.r + 0.2f, pot.g + 0.2f, pot.b + 0.2f);
-            bendages.color = ben;
+            Potion();
         }
         selected = 3;
         if (Input.GetKeyDown("4"))
         {
             selected = 4;
-            sword.color = sw;
-            secondary.color = snd;
-            potion.color = pot;
-            bendages.color = new Color(ben.r + 0.2f, ben.g + 0.2f, ben.b + 0.2f);
+            Bendages();
         }
+    }
 
-
-
+    public void Sword()
+    {
+        sword.color = new Color(sw.r + 0.2f, sw.g + 0.2f, sw.b + 0.2f);
+        secondary.color = snd;
+        potion.color = pot;
+        bendages.color = ben;
+        weapon1.SetActive(true);
+        weapon2.SetActive(false);
+        item1.SetActive(false);
+        item2.SetActive(false);
+    }
+    public void SecondWeapon()
+    {
+        sword.color = sw;
+        secondary.color = new Color(snd.r + 0.2f, snd.g + 0.2f, snd.b + 0.2f);
+        potion.color = pot;
+        bendages.color = ben;
+        weapon1.SetActive(false);
+        weapon2.SetActive(true);
+        item1.SetActive(false);
+        item2.SetActive(false);
+    }
+    public void Potion()
+    {
+        sword.color = sw;
+        secondary.color = snd;
+        potion.color = new Color(pot.r + 0.2f, pot.g + 0.2f, pot.b + 0.2f);
+        bendages.color = ben;
+        weapon1.SetActive(false);
+        weapon2.SetActive(false);
+        item1.SetActive(true);
+        item2.SetActive(false);
+    }
+    public void Bendages()
+    {
+        sword.color = sw;
+        secondary.color = snd;
+        potion.color = pot;
+        bendages.color = new Color(ben.r + 0.2f, ben.g + 0.2f, ben.b + 0.2f);
+        weapon1.SetActive(false);
+        weapon2.SetActive(false);
+        item1.SetActive(false);
+        item2.SetActive(true);
     }
 }
