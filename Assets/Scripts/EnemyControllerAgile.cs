a44225ac3576944832f2a82c0754973bc5bca497
atzoriandrea <37103069+atzoriandrea@users.noreply.github.com>
2019-04-25 20:34:14 +0200

    aggiunti prefab nemici di vario tipo

---
+++ Assets/Scripts/EnemyControllerAgile.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControllerAgile : MonoBehaviour {
+
+    public Transform player;
+    public float walkingDistance = 25.0f;
+    public float smoothTime = 1.0f;
+    private Vector3 smoothVelocity = Vector3.zero;
+    Vector3 movement;
+
+    Animator anim;
+    float distance;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        player = GameObject.Find("Character_Hero_Knight_Male").transform;
+    }
+    void Update()
+    {
+        
+        if (EnemyInfo._health > 0) {
+            //il nemico è sempre rivolto verso il giocatore
+            transform.LookAt(player);
+            //distanza tra nemico e giocatore
+            distance = Vector3.Distance(transform.position, player.position);
+            //camminata nemico
+            if (distance < walkingDistance && distance > 2)
+            {
+                //Il nemico viene sempre verso il giocatore
+                transform.position = Vector3.SmoothDamp(transform.position, player.position, ref smoothVelocity, smoothTime);
+                anim.SetBool("walking", true);
+                anim.SetBool("running", false);
+
+            }
+            else if (distance < walkingDistance * 8 && distance > walkingDistance) {
+                transform.position = Vector3.SmoothDamp(transform.position, player.position, ref smoothVelocity, smoothTime);
+                anim.SetBool("running", true);
+                anim.SetBool("walking", true);
+
+            }
+            else if (distance < 2) {
+                int temp;
+                temp = Random.Range(0, 50);
+                if (temp == 0)
+                    anim.SetTrigger("swordattack");
+
+                anim.SetBool("running", false);
+                anim.SetBool("walking", false);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyInfoAgile.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyInfoAgile : MonoBehaviour {
+
+    public static int _health;
+    Animator anim;
+   private CharacterController _enemyCont;
+    void Start()
+    {
+        _enemyCont = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+        _health = 80;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name.Equals("swordcollider") && PlayerController.attacking)
+        {
+            Debug.Log("hit");
+            Hurt(50);
+        }
+    }
+   
+    public void Hurt(int damage)
+    {
+        _health -= damage;
+        if (_health <= 0)
+        {
+            anim.SetTrigger("death");
+            anim.SetBool("isDead", true);
+            _enemyCont.height = 0;
+            _enemyCont.radius = 0;
+        }
+    }
+
+    public static int getHealth() {
+        return _health;
+    }
+}
