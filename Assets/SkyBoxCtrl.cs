dbc631bf421d6d7c0b56592b31ec69342e29ee99
atzoriandrea <37103069+atzoriandrea@users.noreply.github.com>
2019-05-20 03:22:50 +0200

    fixato switch scene

--- Assets/SkyBoxCtrl.cs
+++ Assets/SkyBoxCtrl.cs
@@ -4,26 +4,34 @@ using UnityEngine;
 
 public class SkyBoxCtrl : MonoBehaviour {
     public Material sky1;
+    public AudioClip scene1;
     public Material sky2;
+    public AudioClip scene2;
     public Material sky3;
+    public AudioClip scene3;
     public Material sky4;
+    public AudioClip scene4;
     public Light flash;
     int scene;
+    public GameObject camera;
+    AudioSource source;
     Transform t;
     //public GameObject luce;
     public Light lt;
     // Use this for initialization
     void Start () {
-        scene = 1 ;
+        scene = 9 ;
         sky1 = RenderSettings.skybox;
-
+        source = camera.GetComponent<AudioSource>();
+        source.volume = 0.1f;
+        
     }
 	
 	// Update is called once per frame
 	void Update () {
         t = GetComponent<Transform>();
         if (t.position.x > 450)
-        {
+        {  
             if (scene != 2)
             {
                 scene = 2;
@@ -32,6 +40,7 @@ public class SkyBoxCtrl : MonoBehaviour {
         }
         if (t.position.x <= 450)
         {
+            
             if (scene != 1)
             {
                 scene = 1; 
@@ -40,38 +49,80 @@ public class SkyBoxCtrl : MonoBehaviour {
         }
         Debug.Log(scene);
     }
+    
           
     IEnumerator changeToSkyOne() {
-        while (flash.intensity <= 100)
+        bool up = true;
+        bool done = false;
+        while (!done)
         {
-            flash.intensity += 15f;
-            lt.intensity = 0;
+            if (up)
+            {
+                if (flash.intensity < 100)
+                {
+                    flash.intensity += 15f;
+                    lt.intensity = 0;
+                }
+                if (flash.intensity >= 100)
+                {
+                    source.clip = scene1;
+                    source.Play();
+                    RenderSettings.skybox = sky1;
+                    up = false;
+                }
+            }
+            else {
+                if (flash.intensity > 0)
+                {
+                    flash.intensity -= 15f;
+                    lt.intensity = 1;
+                }
+                else if (flash.intensity <= 0)
+                {
+                    done = true;
+                }
+
+            }
             yield return null;
         }
-        yield return null;
-        while (flash.intensity >= 0)
-        {
-            RenderSettings.skybox = sky1;
-            flash.intensity -= 15f; 
-            lt.intensity = 1;
-            yield return null;
-        }
+        
+        
         
     }
     IEnumerator changeToSkyTwo()
     {
-        while (flash.intensity <= 100)
+        bool up = true;
+        bool done = false;
+        while (!done)
         {
-            flash.intensity += 15f;
-            lt.intensity = 1;
-            yield return null;
-        }
-        yield return null;
-        while (flash.intensity >= 0)
-        {
-            RenderSettings.skybox = sky2;
-            flash.intensity -= 15f;
-            lt.intensity = 0;
+            if (up)
+            {
+                if (flash.intensity < 100)
+                {
+                    flash.intensity += 15f;
+                    lt.intensity = 1;
+                }
+                if (flash.intensity >= 100)
+                {
+                    source.clip = scene2;
+                    source.Play();
+                    RenderSettings.skybox = sky2;
+                    up = false;
+                }
+            }
+            else
+            {
+                if (flash.intensity > 0)
+                {
+                    flash.intensity -= 15f;
+                    lt.intensity = 0;
+                }
+                else if(flash.intensity <= 0)
+                {
+                    done = true;
+                }
+
+            }
             yield return null;
         }
 
